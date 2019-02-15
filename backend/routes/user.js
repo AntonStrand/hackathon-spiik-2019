b7f5ecad6a9cff1d7ee3b7861a08d38052755c19
William Robertson <c.william.robertson@gmail.com>
2019-02-15 23:13:35 +0100

    can now add a child

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,19 +1,32 @@
 const router = require('express').Router()
+const child = require('../models/children')
 
 router.get('/', (req, res) => {
   res.json('this is user')
 })
 
-router.get('/:userId', (req, resp) => {
+router.get('/:userId', (req, res) => {
   const userId = req.params.userId
 
   console.log(userId)
 })
 
-router.get('/:userId/children', (req, resp) => {
+router.get('/:userId/children', (req, res) => {
   const userId = req.params.userId
 
   console.log(userId + ' these are my children')
+})
+
+router.post('/:userId/children', (req, res) => {
+  const userId = req.params.userId
+  const body = req.body
+  const childName = body.childName
+
+  const fooBar = new child({ name: childName, parentID: userId, saldo: 0 })
+
+  fooBar.save().then(() => {
+    res.send()
+  })
 })
 
 module.exports = router
