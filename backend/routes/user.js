83aa495637605618db4685b634bbeeaf31883de6
Elmona <emil_larsson83@hotmail.com>
2019-02-15 23:23:29 +0100

    Adding new accounts

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,29 @@
 const router = require('express').Router()
+const Accounts = require('../models/accounts')
 
 router.get('/', (req, res) => {
   res.json('this is user')
+})
+
+router.post('/', (req, res) => {
+  const { username, password, email } = req.body
+
+  if (!username || !password || !email) {
+    res.status(401).send('Invalid input')
+  }
+
+  new Accounts({ name: username, password: password, email: email })
+    .save()
+    .then(data => {
+      res.json('Added user')
+    })
+    .catch(e => {
+      if (e.code === 11000) {
+        res.json('User already exist')
+      } else {
+        res.json('Unknown error')
+      }
+    })
 })
 
 router.get('/:userId', (req, resp) => {
