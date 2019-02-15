7dcf43d87814df8421191c218a723241365e118c
William Robertson <c.william.robertson@gmail.com>
2019-02-15 21:27:58 +0100

    added multiple routes

--- backend/routes/child.js
+++ backend/routes/child.js
@@ -10,4 +10,22 @@ router.get('/:childId', (req, res) => {
   console.log(childId)
 })
 
+router.get('/:childId/wallet', (req, res) => {
+  const childId = req.params.childId
+
+  console.log(childId)
+})
+
+router.get('/:childId/wishlist', (req, res) => {
+  const childId = req.params.childId
+
+  console.log(childId)
+})
+
+router.get('/:childId/history', (req, res) => {
+  const childId = req.params.childId
+
+  console.log(childId)
+})
+
 module.exports = router
