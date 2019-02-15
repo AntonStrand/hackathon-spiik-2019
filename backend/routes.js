817e0d5ed28b25fe63c2117bb61e9c53178661b0
Elmona <emil_larsson83@hotmail.com>
2019-02-16 00:08:57 +0100

    Sedning jsonwebtoken on sucessfull login

--- backend/routes.js
+++ backend/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const index = require('./routes/index')
 const user = require('./routes/user')
 const child = require('./routes/child')
+const login = require('./routes/login')
 
 router.use('/', index)
 router.use('/user', user)
 router.use('/child', child)
+router.use('/login', login)
 
 module.exports = router
