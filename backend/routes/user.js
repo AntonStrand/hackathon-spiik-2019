dd5ab12bf24e1088851c47386597bbd3958a4692
William Robertson <c.william.robertson@gmail.com>
2019-02-15 23:44:59 +0100

    removed userid param

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,19 +5,13 @@ router.get('/', (req, res) => {
   res.json('this is user')
 })
 
-router.get('/:userId', (req, res) => {
-  const userId = req.params.userId
-
-  console.log(userId)
-})
-
-router.get('/:userId/children', (req, res) => {
+router.get('/children', (req, res) => {
   const userId = req.params.userId
 
   console.log(userId + ' these are my children')
 })
 
-router.post('/:userId/children', (req, res) => {
+router.post('/children', (req, res) => {
   const userId = req.params.userId
   const body = req.body
   const childName = body.childName
