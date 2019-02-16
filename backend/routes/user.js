99024522de03646720f6eddaaafa4f57620fe9c1
Maddis87 <madeleneamberman@gmail.com>
2019-02-16 11:18:18 +0100

    added functionality to get children from database

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -31,23 +31,31 @@ router.post('/', (req, res) => {
 })
 
 router.get('/children', passportJWT, (req, res) => {
-  console.log('sdlfkjsd')
-  const userId = req.params.userId
-
-  console.log(userId + ' these are my children')
-  res.send('wohooo')
+  const {id} = req.user
+  Accounts.findOne({_id: id}).then(data => {
+    const result = data.children.map(child => {
+      console.log(child)
+      return ChildModel.findOne({ _id: child })
+    })
+    Promise.all(result).then(data => res.json(data))
+  })
 })
 
-router.post('/children', (req, res) => {
-  const userId = req.params.userId
-  const body = req.body
-  const childName = body.childName
+router.post('/children', passportJWT, (req, res) => {
+  const { id } = req.user
+  const childName = req.body.childName
 
-  const child = new ChildModel({ name: childName, parentID: userId, saldo: 0 })
-
-  child.save().then(() => {
-    res.send()
-  })
+  if (!childName) {
+    res.json('No child name')
+  } else {
+    new ChildModel({ name: childName, parentID: id, saldo: 0 })
+      .save().then(data => {
+        const { _id } = data
+        return Accounts.findOneAndUpdate({ _id: id }, {$addToSet: { children: _id }})
+      }).then(data => {
+        res.send('Child added!')
+      })
+  }
 })
 
 module.exports = router
