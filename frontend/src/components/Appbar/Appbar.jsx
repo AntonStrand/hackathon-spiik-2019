5b969864a5d5bbaf0ed2bbe323cc7baaf545ee3f
William Robertson <c.william.robertson@gmail.com>
2019-02-16 11:55:09 +0100

    Has a working button

--- frontend/src/components/Appbar/Appbar.jsx
+++ frontend/src/components/Appbar/Appbar.jsx
@@ -3,16 +3,21 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
 class Appbar extends React.Component {
   render() {
     return (
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ justifyContent: 'space-between' }}>
           <Typography variant="h6" color="inherit">
             Mina pengar
           </Typography>
-          <Button color="inherit">Login/Register</Button>
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="secondary">
+              Login/Register
+            </Button>
+          </Link>
         </Toolbar>
       </AppBar>
     )
