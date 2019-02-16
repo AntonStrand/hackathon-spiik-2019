3c8f102bd9f425eeba5396cff84ce4cfcf126055
William Robertson <c.william.robertson@gmail.com>
2019-02-16 10:52:55 +0100

    added loginPAge

---
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>login page!</div>
+      </div>
+    )
+  }
+}
+
+export default LoginPage
