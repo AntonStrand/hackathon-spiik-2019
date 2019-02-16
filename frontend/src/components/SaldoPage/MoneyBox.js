9e48d7ec759fe848b8918b4b35130b1fe2f5fee5
antonstrand <jag@antonstrand.se>
2019-02-16 15:51:31 +0100

    Add sound effects

--- frontend/src/components/SaldoPage/MoneyBox.js
+++ frontend/src/components/SaldoPage/MoneyBox.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Coin from './money/Coin'
+import CoinClickEffect from './money/sound-effects/coin-click.mp3'
 import Bill from './money/Bill'
+import BillClickEffect from './money/sound-effects/bill-click.mp3'
 
 const denomination = sum => (sum > 10 ? Bill : Coin)
+const sound = sum =>
+  sum > 10 ? new Audio(BillClickEffect) : new Audio(CoinClickEffect)
 
 const Container = styled.div`
   background: #eee;
@@ -27,7 +31,13 @@ const MoneyBox = ({ moneyList, onMoveMoney }) => (
         .map(m => {
           const Denomination = denomination(m)
           return (
-            <Denomination key={Math.random()} onClick={() => onMoveMoney(m)}>
+            <Denomination
+              key={Math.random()}
+              onClick={() => {
+                sound(m).play()
+                onMoveMoney(m)
+              }}
+            >
               {m}
             </Denomination>
           )
