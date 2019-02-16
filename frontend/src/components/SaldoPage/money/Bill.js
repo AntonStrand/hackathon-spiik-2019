e854935bdf847c55095cff4271ed816121036e83
antonstrand <jag@antonstrand.se>
2019-02-16 10:31:47 +0100

    different colors depending on bill value

--- frontend/src/components/SaldoPage/money/Bill.js
+++ frontend/src/components/SaldoPage/money/Bill.js
@@ -1,17 +1,36 @@
 import styled from 'styled-components'
 
+// prettier-ignore
+// getBackgroundColor :: Props -> HexCode
+const getBackgroundColor = ({ children: value }) =>
+  value === 20 ? '#AB99B9'
+  : value === 50 ? '#F0E0BC'
+  : value === 100 ? '#BBD2E5'
+  : value === 200 ? '#BFEEC9'
+  : value === 500 ? '#F3C1B8'
+  : /* otherwise */ '#E1CCCC'
+
+// prettier-ignore
+// getTextColor :: Props -> HexCode
+const getTextColor = ({ children: value }) =>
+  value === 20 ? '#59466E'
+  : value === 50 ? '#D86337'
+  : value === 100 ? '#426E9B'
+  : value === 200 ? '#56a267'
+  : value === 500 ? '#D04337'
+  : /* otherwise */ '#BF8282'
+
 const Bill = styled.div`
   user-select: none;
-  background: #bfeec9;
+  background: ${getBackgroundColor};
   border-radius: 2px;
-  border: 1px solid #a1dbae;
   text-align: center;
   height: 54px;
   width: 106px;
   line-height: 54px;
   font-size: 16px;
   font-weight: bold;
-  color: #56a267;
+  color: ${getTextColor};
   box-shadow: 0 1px 1px #999;
   cursor: pointer;
   transition: all 100ms;
