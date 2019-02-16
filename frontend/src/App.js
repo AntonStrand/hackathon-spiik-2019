90409fd192c5d8bf76db62e84f941fea686ddc0e
William Robertson <c.william.robertson@gmail.com>
2019-02-16 11:04:27 +0100

    Uses Route to route user to correct page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router'
 import './App.css'
-
 import SaldoPage from './components/SaldoPage/'
+import LoginPage from './components/LoginPage/LoginPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SaldoPage />
+        <Route exact path="/" component={SaldoPage} />
+        <Route path="/login" component={LoginPage} />
       </div>
     )
   }
